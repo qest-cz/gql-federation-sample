58a0f312c048bf726e14cbebe9bc9a0db45e1f0f
Jakub Mikes <jakub.mikes@qest.cz>
2022-08-05 12:05:13 +0200

    cz removed

--- monorepo/monorepo-ws/apps/userapp/src/Schema/user/index.test.ts
+++ monorepo/monorepo-ws/apps/userapp/src/Schema/user/index.test.ts
@@ -1,7 +1,7 @@
-import * as funkce from './index'
+import { getUsers } from './index'
 describe('test users', () => {
     test('count array is 4', () => {
-        const len: number = funkce.getUsers().length
+        const len: number = getUsers().length
         expect(len).toBe(4)
     })
 })
