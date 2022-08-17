fbc650437878a4eff335660a0c21e192098575ed
Jakub Mikes <jakub.mikes@qest.cz>
2022-08-17 15:14:13 +0200

    add supergraph to federation gateway now federation-gateway can start without run user and article app

--- monorepo/monorepo-ws/apps/federation-gateway/src/main.ts
+++ monorepo/monorepo-ws/apps/federation-gateway/src/main.ts
@@ -1,11 +1,15 @@
 import { ApolloServer } from 'apollo-server';
 import { ApolloGateway } from '@apollo/gateway';
+import { readFileSync } from 'fs';
 
+
+const superGraphSchema = readFileSync('./apps/federation-gateway/src/supergraph.graphql').toString()
 const gateway = new ApolloGateway({
-  serviceList: [
-    { name: 'users', url: process.env.URL_USER_APP },
-    { name: 'articles', url: process.env.URL_ARTICLE_APP },
-  ],
+  // serviceList: [
+  //   { name: 'users', url: process.env.URL_USER_APP },
+  //   { name: 'articles', url: process.env.URL_ARTICLE_APP },
+  // ],
+  supergraphSdl: superGraphSchema
 });
 
 const server = new ApolloServer({ gateway });
